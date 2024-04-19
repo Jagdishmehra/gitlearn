4a3aa5f89bc18e27e0e3fad7863ee16ff7fcca49
jagdish mehra <jagdishs.mehra@outlook.com>
2024-04-19 11:44:23 +0530

    to find nth term of series

---
+++ tcsprp/findntrm.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<math.h>
+#include<vector>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    if(n%2!=0)
+    {
+        cout<<pow(2,(n/2))<<endl;
+    }
+    else
+    {
+        cout<<pow(3,(n/2)-1)<<endl;
+    }
+}
